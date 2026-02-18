1112f213a9a268d8eaf243de2c22537f8679b7bf
Harsh Patial <harsh.work211@gmail.com>
2026-02-18 12:37:28 +0530

    Time: 31 ms (35.35%), Space: 47.6 MB (28.79%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -57,3 +57,33 @@ class Solution {
         return res;
     }
 }
+
+//BRUTE FORCE
+
+// class Solution {
+//     static boolean areanagram(String s1, String s2){
+//         int[] cnt = new int[26];
+//         for(int i=0; i<s1.length(); i++){
+//             cnt[s1.charAt(i) - 'a']++;
+//         }
+//         for(int i=0; i<s2.length(); i++){
+//             cnt[s2.charAt(i) - 'a']--;
+//         }
+//         for(int i=0; i<26; i++){
+//             if(cnt[i] != 0)
+//             return false;
+//         }
+//         return true;
+//     }
+//     public List<Integer> findAnagrams(String s, String p) {
+//         List<Integer> res = new ArrayList<>();
+
+//         for(int i=0; i<s.length()-p.length()+1; i++){
+//             if(areanagram(s.substring(i, i+p.length()),p)){
+//                 res.add(i);
+//             }
+//         }
+//         return res;
+//     }
+
+// }
