06c98076f47304426372357e31f92f5ffcff7408
Harsh Patial <harsh.work211@gmail.com>
2026-02-22 12:51:17 +0530

    Time: 1 ms (100%), Space: 42.7 MB (99.85%) - LeetHub

---
+++ 3848-check-digitorial-permutation/3848-check-digitorial-permutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isDigitorialPermutation(int n) {
+        int num = n;
+        int sum = 0;
+        while (num > 0){
+            int m =  num%10;
+            num = num/10;
+            
+            sum += fact(m);
+            }
+            //permutation of number n | if ( a == sum ) return true; |else return false; --> TC inc
+        
+        int[] freq1 = new int[10];
+        int temp = n;
+        while(temp > 0){
+            freq1[temp % 10]++;
+            temp /= 10;
+        }
+
+        int[] freq2 = new int[10];
+        temp = sum;
+        while(temp > 0){
+            freq2[temp % 10]++;
+            temp /= 10;
+        }
+        for(int i = 0; i < 10; i++){
+            if(freq1[i] != freq2[i]){
+            return false;
+            }
+        }
+        return true;
+    }
+    public int fact(int k) {
+        if (k == 0) {
+            return 1;
+        }
+        int fac = k * fact(k - 1);
+        return fac;
+    }
+}
