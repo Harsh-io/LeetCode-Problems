6a75876a5d8f858ebf6f34f9d6d8d613597e9836
Harsh Patial <harsh.work211@gmail.com>
2026-02-22 12:00:31 +0530

    Time: 2 ms (18.19%), Space: 47.2 MB (23.25%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
@@ -1,42 +1,71 @@
+// //BRUTE FORCE
+// class Solution {
+//     public int scoreDifference(int[] arr) {
+//         boolean P1_Active = true;
+//         boolean P2_Active = false;
+//         int run1 = 0, run2 = 0;
+
+//         for(int i=0; i<arr.length; i++){
+            
+//             if(arr[i]%2 != 0){
+//                 //switch a and b
+//                 if(P1_Active){
+//                     P1_Active = false;
+//                     P2_Active = true;
+//                 }
+//                 else{
+//                     P1_Active = true;
+//                     P2_Active = false;
+//                 }
+//             }
+            
+//             if((i+1)%6 == 0){
+//                 //switch a and b
+//                 if(P1_Active){
+//                     P1_Active = false;
+//                     P2_Active = true;
+//                 }
+//                 else{
+//                     P1_Active = true;
+//                     P2_Active = false;
+//                 }
+//             }
+            
+//             if(P1_Active){
+//                 run1 += arr[i];
+//             }
+//             else{
+//                 run2+= arr[i];
+//             }
+//         }
+//         return run1-run2;
+//     }
+// }
+
+//OPTIMIZED
+
 class Solution {
     public int scoreDifference(int[] arr) {
-        boolean P1_Active = true;
-        boolean P2_Active = false;
-        int run1 = 0, run2 = 0;
+        boolean isP1Active = true;
+        int diff = 0;
 
         for(int i=0; i<arr.length; i++){
             
             if(arr[i]%2 != 0){
-                //switch a and b
-                if(P1_Active){
-                    P1_Active = false;
-                    P2_Active = true;
-                }
-                else{
-                    P1_Active = true;
-                    P2_Active = false;
-                }
+                isP1Active = !isP1Active;
             }
-            
+
             if((i+1)%6 == 0){
-                //switch a and b
-                if(P1_Active){
-                    P1_Active = false;
-                    P2_Active = true;
-                }
-                else{
-                    P1_Active = true;
-                    P2_Active = false;
-                }
+                isP1Active = !isP1Active;
             }
             
-            if(P1_Active){
-                run1 += arr[i];
+            if(isP1Active){
+                diff += arr[i];
             }
             else{
-                run2+= arr[i];
+                diff -= arr[i];
             }
         }
-        return run1-run2;
+        return diff;
     }
 }
