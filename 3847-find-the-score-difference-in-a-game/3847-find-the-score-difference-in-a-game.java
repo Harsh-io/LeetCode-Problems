c950de2f769bf8fa06e43c0ea9d54b0d26395fd4
Harsh Patial <harsh.work211@gmail.com>
2026-02-22 11:54:51 +0530

    Time: 2 ms (18.19%), Space: 47.2 MB (44.67%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int scoreDifference(int[] arr) {
+        boolean P1_Active = true;
+        boolean P2_Active = false;
+        int run1 = 0, run2 = 0;
+
+        for(int i=0; i<arr.length; i++){
+            
+            if(arr[i]%2 != 0){
+                //switch a and b
+                if(P1_Active){
+                    P1_Active = false;
+                    P2_Active = true;
+                }
+                else{
+                    P1_Active = true;
+                    P2_Active = false;
+                }
+            }
+            
+            if((i+1)%6 == 0){
+                //switch a and b
+                if(P1_Active){
+                    P1_Active = false;
+                    P2_Active = true;
+                }
+                else{
+                    P1_Active = true;
+                    P2_Active = false;
+                }
+            }
+            
+            if(P1_Active){
+                run1 += arr[i];
+            }
+            else{
+                run2+= arr[i];
+            }
+        }
+        return run1-run2;
+    }
+}
