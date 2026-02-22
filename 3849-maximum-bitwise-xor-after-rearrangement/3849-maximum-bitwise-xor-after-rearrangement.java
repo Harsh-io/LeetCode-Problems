0f3da01c79eff78d31c1b0a1bfd885d85aed60e0
Harsh Patial <harsh.work211@gmail.com>
2026-02-22 14:18:25 +0530

    Time: 64 ms (70.71%), Space: 55.9 MB (54.93%) - LeetHub

--- 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.java
+++ 3849-maximum-bitwise-xor-after-rearrangement/3849-maximum-bitwise-xor-after-rearrangement.java
@@ -1,39 +1,41 @@
 // class Solution {
 //     public String maximumXor(String s, String t) {
+
 //         char[] arr1 = s.toCharArray();
 //         char[] arr2 = t.toCharArray();
+
 //         int cnt1 = 0;
 //         int cnt0 = 0;
 
-//         for(int j=0; j<arr2.length; j++){
-//             if(arr2[j] == '1'){cnt1++;}
-//             else{cnt0++;}
+//         // Count bits in t
+//         for(int j = 0; j < arr2.length; j++){
+//             if(arr2[j] == '1') cnt1++;
+//             else cnt0++;
 //         }
-//         for(int i=0; i<arr1.length; i++){
-//             if (arr1[i] == '0'){
-//                 if(cnt1 != 0){
-//                     arr2[i] = 1;
+
+//         // Rearrange t to maximize XOR
+//         for(int i = 0; i < arr1.length; i++){
+//             if(arr1[i] == '0'){
+//                 if(cnt1 > 0){
+//                     arr2[i] = '1';
 //                     cnt1--;
-//                 }
-//                 else{
-//                     arr2[i] = 0;
+//                 } else {
+//                     arr2[i] = '0';
 //                     cnt0--;
 //                 }
-//             }
-//             else{
-//                 if(cnt0 != 0){
-//                     arr2[i] = 0;
+//             } 
+//             else { // arr1[i] == '1'
+//                 if(cnt0 > 0){
+//                     arr2[i] = '0';
 //                     cnt0--;
-//                 }
-//                 else{
-//                     arr2[i] = 1;
+//                 } else {
+//                     arr2[i] = '1';
 //                     cnt1--;
 //                 }
 //             }
 //         }
 
-//         // perform XOR on S and T
-//                 StringBuilder result = new StringBuilder();
+//         StringBuilder result = new StringBuilder();
 
 //         for(int i = 0; i < arr1.length; i++){
 //             int bit1 = arr1[i] - '0';
@@ -47,53 +49,41 @@
 //         return result.toString();
 //     }
 // }
+
 class Solution {
     public String maximumXor(String s, String t) {
 
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        int ones = 0;
 
-        int cnt1 = 0;
-        int cnt0 = 0;
-
-        // Count bits in t
-        for(int j = 0; j < arr2.length; j++){
-            if(arr2[j] == '1') cnt1++;
-            else cnt0++;
+        // Count 1s in t
+        for(char c : t.toCharArray()){
+            if(c == '1') ones++;
         }
 
-        // Rearrange t to maximize XOR
-        for(int i = 0; i < arr1.length; i++){
-            if(arr1[i] == '0'){
-                if(cnt1 > 0){
-                    arr2[i] = '1';
-                    cnt1--;
-                } else {
-                    arr2[i] = '0';
-                    cnt0--;
-                }
-            } 
-            else { // arr1[i] == '1'
-                if(cnt0 > 0){
-                    arr2[i] = '0';
-                    cnt0--;
-                } else {
-                    arr2[i] = '1';
-                    cnt1--;
-                }
-            }
-        }
+        int zeros = t.length() - ones;
 
-        // 🔥 Perform XOR
         StringBuilder result = new StringBuilder();
 
-        for(int i = 0; i < arr1.length; i++){
-            int bit1 = arr1[i] - '0';
-            int bit2 = arr2[i] - '0';
+        for(int i = 0; i < s.length(); i++){
 
-            int xor = bit1 ^ bit2;
-
-            result.append(xor);
+            if(s.charAt(i) == '0'){
+                if(ones > 0){
+                    result.append('1');
+                    ones--;
+                } else {
+                    result.append('0');
+                    zeros--;
+                }
+            }
+            else { // s[i] == '1'
+                if(zeros > 0){
+                    result.append('1');
+                    zeros--;
+                } else {
+                    result.append('0');
+                    ones--;
+                }
+            }
         }
 
         return result.toString();
