9d832c114751f6bd768c8c63ca2dd5af4df7f6c7
Harsh Patial <harsh.work211@gmail.com>
2026-03-02 19:01:00 +0530

    Time: 2 ms (99.22%), Space: 47.1 MB (73.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -12,6 +12,9 @@ class Solution {
         return new int[0];
     }
 }
+//TC: O(NLOGN)
+//SC: O(N)
+
 //BRUTE FORCE
         // for(int i=0; i<arr.length-1; i++){
         //     for(int j=i+1; j<arr.length; j++){
@@ -21,7 +24,33 @@ class Solution {
         //     }
         // }
         // return new int[0];
-        // }
+        // } 
+        //TC: O(N2); SC: 0(1)
+
+//Greedy Approach + 2  Pointer 
+// int n = arr.length;
+// int[][] Original = new int[n][2];
+
+// for(int i=0; i<arr.length; i++){
+//     Original[i][0] = arr[i];
+//     Original[i][1] = i;
+// }
+// Arrays.sort(Original, (a,b) -> Integer.compare(a[0], b[0]));
+// int i=0, j = n-1;
+// while (i < j) {
+//             int sum = Original[i][0] + Original[j][0];
+//             if (sum == target) {
+//                 return new int[] {Original[i][1], Original[j][1]};
+//             } else if (sum < target) {
+//                 i++;
+//             } else {
+//                 j--;
+//             }
+//         }
+//         return new int[] {-1, -1};
+//     }
+// }
+//TC:O(N) | SC: O(n) 
 
 
 
