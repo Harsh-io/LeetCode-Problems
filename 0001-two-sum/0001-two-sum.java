5f3f6ca57ce87d0ce9954c00ab37ddb93c49dfea
Harsh Patial <harsh.work211@gmail.com>
2026-03-02 18:53:18 +0530

    Time: 2 ms (99.22%), Space: 47.3 MB (21.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,27 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] arr, int target){
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[i] + arr[j] == target){
-                    return new int[]{i,j};
-                }
-            }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            int b = target - arr[i];
+            if(map.containsKey(b))
+                return new int[]{map.get(b),i};
+            map.put(arr[i],i);
         }
         return new int[0];
-        }
+    }
 }
-
-// OPTIMAL ONE
-//     public int[] twoSum(int[] nums, int target) {
-//         Map<Integer, Integer> map = new HashMap<>();
-//         for (int i = 0; i < nums.length; i++) {
-//             int diff = target - nums[i];
-//             if (map.containsKey(diff))
-//                 return new int[] { map.get(diff), i };
-//             map.put(nums[i], i);
-//         }
-//         return new int[0];
+//BRUTE FORCE
+        // for(int i=0; i<arr.length-1; i++){
+        //     for(int j=i+1; j<arr.length; j++){
+        //         if(arr[i] + arr[j] == target){
+        //             return new int[]{i,j};
+        //         }
+        //     }
+        // }
+        // return new int[0];
+        // }
 
 
 
