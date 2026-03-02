620c729aa84b2e45bc8ff12cb47e895092c23d1f
Harsh Patial <harsh.work211@gmail.com>
2026-03-02 19:41:08 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.67%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int c0=0, c1=0, c2=0;
+        for(int num : arr){
+            if(num == 0){c0++;}
+            else if(num == 1){c1++;}
+            else{c2++;}
+        }
+        for(int i=0; i<c0; i++){arr[i] = 0;}
+        for(int i=c0; i<c0+c1; i++){arr[i] = 1;}
+        for(int i=c0+c1; i<arr.length; i++){arr[i] = 2;}
+    }
+}
+
+
+//  int a = 0, b = arr.length - 1;
+//         int i = 0;
+
+//         while (i <= b) {
+//             if (arr[i] == 0) {
+//                 int temp = arr[a];
+//                 arr[a] = arr[i];
+//                 arr[i] = temp;
+//                 a++;
+//                 i++;
+//             } 
+//             else if (arr[i] == 2) {
+//                 int temp = arr[b];
+//                 arr[b] = arr[i];
+//                 arr[i] = temp;
+//                 b--;
+//             } 
+//             else {
+//                 i++;
+//             }
+//         }
