081d63af39c960ae57d6ce057755aded4ac1e594
Harsh Patial <harsh.work211@gmail.com>
2026-03-04 14:01:42 +0530

    Time: 1 ms (99.77%), Space: 94.7 MB (30.51%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,39 @@
+
+class Solution {
+    public static int maxProfit(int[] arr) {
+
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] < min) {min = arr[i];}
+            
+            int profit = arr[i] - min;
+
+            if(profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+}
+
+//BRUTE FORCE 
+// class Solution {
+//     public static int maxProfit(int[] arr) {
+// 		int p = 0;
+// 		int maxp = 0;
+// 		int tp = 0;
+		
+// 		for(int i=0; i<arr.length; i++){
+// 		    for(int j=i+1; j<arr.length; j++){
+// 		        if(arr[i] < arr[j]){
+// 		            tp = Math.max(p, (arr[j] - arr[i]));
+// 		        }
+// 		        maxp = Math.max(tp, maxp);
+// 		    }
+// 		}
+// 		return maxp;
+		
+// 	}
+// }
