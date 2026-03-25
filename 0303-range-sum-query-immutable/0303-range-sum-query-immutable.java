b9c98c67d4cf74a77e2c34515fb102020914f607
Harsh Patial <harsh.work211@gmail.com>
2026-03-25 10:49:39 +0530

    Time: 8 ms (71.63%), Space: 47.7 MB (75.63%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,21 +1,44 @@
-class NumArray {
-    int[] arr; //first initialise -> class obj
-    public NumArray(int[] nums) { // constructor 
-        this.arr = nums; //Makes it accessible to all methods as "arr"
-    }
+//Optimal - Prefix sum
+
+//Brute Forcce
+// class NumArray {
+//     int[] arr; //first initialise -> class obj
+//     public NumArray(int[] nums) { // constructor 
+//         this.arr = nums; //Makes it accessible to all methods as "arr"
+//     }
     
-    public int sumRange(int left, int right) {
-        int sum = 0;
-        while(left <= right){
-            sum = sum + arr[left];
-            left++;
-        }
-        return sum;
-    }
-}
+//     public int sumRange(int left, int right) {
+//         int sum = 0;
+//         while(left <= right){
+//             sum = sum + arr[left];
+//             left++;
+//         }
+//         return sum;
+//     }
+// }
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
  * int param_1 = obj.sumRange(left,right);
  */
+
+class NumArray {
+    int[] prefix; 
+
+    public NumArray(int[] nums){
+        prefix = new int[nums.length];
+
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+    }
+    public int sumRange(int left, int right) {
+       if (left == 0) {
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
+    }
+}
