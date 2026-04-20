21014227bdcafbde8422f0f28a40154678f24c2b
Harsh Patial <harsh.work211@gmail.com>
2026-04-20 19:13:20 +0530

    Time: 0 ms (100%), Space: 46.5 MB (46.58%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -15,3 +15,22 @@ class Solution {
         return ans;
     }
 }
+
+//TRY Doing this  using swapping
+// class Solution {
+//     public int[] shuffle(int[] nums, int n) {
+//         for (int i = 0; i < n; i++) {
+//             int j = n + i;   // position of y[i]
+
+//             int temp = nums[j];
+
+//             // shift elements right to make space
+//             for (int k = j; k > 2 * i + 1; k--) {
+//                 nums[k] = nums[k - 1];
+//             }
+
+//             nums[2 * i + 1] = temp;
+//         }
+//         return nums;
+//     }
+// }
