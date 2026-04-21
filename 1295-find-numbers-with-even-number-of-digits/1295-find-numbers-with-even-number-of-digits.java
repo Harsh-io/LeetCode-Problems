82b1d4e2db0cce36483716995e95efb9c769a3c2
Harsh Patial <harsh.work211@gmail.com>
2026-04-21 23:49:46 +0530

    Time: 1 ms (99.59%), Space: 44.3 MB (95.46%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+
+        int even = 0;
+        for(int num : nums){
+            int count = 0;
+            int n = num;
+
+            while (n > 0){
+                n = n/10;
+                count++;
+            }
+            if (count%2 ==0){
+                even++;
+            }
+        }
+         return even;
+  }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int[] nums = {12,345,2,6,7896};
+        int result = sol.findNumbers(nums);
+         System.out.println("Count of numbers with even number of digits: " + result);
+    }
+}
+
+
+// ERROR : 
+/*
+Missing return
+Logic Error	% 10 only --> use /
+Declare count = 0; inside the loo
+r = i % 10;	Variable r is never used, and % 10 doesn’t count digits	Use / 10 in a loop
+for i in nums	Invalid Java syntax (this is Python style)	Use for (int i : nums)
+        // result = findNumbers(count, nums);
+
+
+MY CODE 
+    //     for i in nums{
+    //         if (i>0){                      // used in
+    //             r = i % 10;                // used % instead of /
+    //             count += 1;
+    //         }
+    //         if (count%2 == 0){
+    //                 even += 1;
+    //             }
+    //     }
+        
+    // }
+/*
+/*
+psuedo code 
+1) traverse the array
+    - check each element
+    - divide by 10 and get reminder 
+    - store total  no of digits in i 
+    if i%2 === 0 
+    even +=1
+2) repeat until all the ele of array are covered 
+*/
