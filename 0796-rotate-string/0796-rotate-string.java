9b4ad28e064b455eaa79e748d25975ab31bdc209
Harsh Patial <harsh.work211@gmail.com>
2026-05-15 19:05:52 +0530

    Time: 1 ms (24.55%), Space: 43.3 MB (6.55%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,10 +1,29 @@
 class Solution {
-    public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length()) {
-            return false;
-        }
+    public boolean rotateString(String s, String g) {
+        if(s.length() != g.length()){return false;}
 
-        String doubled = s + s;
-        return doubled.contains(goal);
+        for(int i=0; i<s.length(); i++){
+            String r = s.substring(i,s.length()) + s.substring(0,i);
+            if(r.equals(g)){return true;}
+        }
+        return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OPTIMAL
+// String sb = s + s;
+//return sb.contains(g);
