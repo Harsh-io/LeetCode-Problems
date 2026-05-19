443f1b2718815eb6bbd512bfbd86432a13b9c050
Harsh Patial <harsh.work211@gmail.com>
2026-05-20 00:01:59 +0530

    Time: 36 ms (13.02%), Space: 47.4 MB (5.71%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -3,43 +3,26 @@ import java.util.*;
 class Solution {
 
     public int numberOfSubstrings(String s) {
-
         int n = s.length();
-
         int left = 0;
         int count = 0;
 
         HashMap<Character, Integer> map = new HashMap<>();
-
         for (int right = 0; right < n; right++) {
-
             char ch = s.charAt(right);
-
-            // add current character
             map.put(ch, map.getOrDefault(ch, 0) + 1);
 
-            // window contains a,b,c
-            while (map.containsKey('a') &&
-                   map.containsKey('b') &&
-                   map.containsKey('c')) {
-
-                // all future extensions also valid
+            while (map.size() == 3) {
                 count += (n - right);
 
-                // shrink window
                 char leftChar = s.charAt(left);
-
                 map.put(leftChar, map.get(leftChar) - 1);
-
-                // remove if frequency becomes 0
                 if (map.get(leftChar) == 0) {
                     map.remove(leftChar);
                 }
-
                 left++;
             }
         }
-
         return count;
     }
 }
