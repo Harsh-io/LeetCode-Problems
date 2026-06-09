cf9bf7643cbb5c0a75ddc97d0eca2dbcb8c1d88b
Harsh Patial <harsh.work211@gmail.com>
2026-06-09 13:15:15 +0530

    Time: 1 ms (100%), Space: 43.7 MB (74.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,35 +1,36 @@
 class Solution {
     public int myAtoi(String s) {
+        s = s.trim();
+        if(s.length() == 0) return 0;
 
-        String str = s.trim();
-        if(str.length() == 0) return 0;
-
-        long ans = 0;
         int sign = 1;
-        int i = 0;
+        int start = 0;
+        long ans = 0;
 
-        if(str.charAt(0) == '-'){
-            sign = -1;
-            i++;
+        if(s.charAt(start) == '-' || s.charAt(start) == '+'){
+            sign = (s.charAt(start) == '-') ? -1 : 1;
+            start++;
         }
-        else if(str.charAt(0) == '+'){
-            i++;
-        }
-        
-        while(i < str.length() && Character.isDigit(str.charAt(i))){
-            
-            int r = str.charAt(i) - '0';
 
-            ans = ans * 10 + r;
+        return solve(s, start, 0, sign);
+    }
 
-            if(sign * ans > Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
-
-            if(sign * ans < Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
-
-            i++;
-            }
+    private int solve(String s, int i, long ans, int sign){
+        if (i >= s.length() || !Character.isDigit(s.charAt(i))) {
             return (int)(sign * ans);
+        }
+
+        int digit = s.charAt(i) - '0';
+        ans = ans * 10 + digit;
+
+        if(sign * ans > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if(sign * ans < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return solve(s, i+1, ans, sign);
     }
 }
