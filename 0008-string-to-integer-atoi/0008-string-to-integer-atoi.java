9093ad75cf25655cfa5af0e5704ca14fbf7039c5
Harsh Patial <harsh.work211@gmail.com>
2026-06-09 11:53:24 +0530

    Time: 1 ms (100%), Space: 43.9 MB (41.02%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,33 +1,35 @@
 class Solution {
     public int myAtoi(String s) {
 
-        long ans = 0;
-        s = s.strip();
-        int i = 0;
-        int sign = 1;
+        String str = s.trim();
+        if(str.length() == 0) return 0;
 
-        if(i < s.length() && s.charAt(i) == '-') {
+        long ans = 0;
+        int sign = 1;
+        int i = 0;
+
+        if(str.charAt(0) == '-'){
             sign = -1;
             i++;
         }
-        else if(i < s.length() && s.charAt(i) == '+') {
+        else if(str.charAt(0) == '+'){
             i++;
         }
+        
+        while(i < str.length() && Character.isDigit(str.charAt(i))){
+            
+            int r = str.charAt(i) - '0';
 
-        // digit parsing
-        while(i < s.length() && Character.isDigit(s.charAt(i))) {
-            int r = s.charAt(i) - '0';
-            ans = 10 * ans + r;
+            ans = ans * 10 + r;
 
-            if(sign * ans > Integer.MAX_VALUE) {
+            if(sign * ans > Integer.MAX_VALUE)
                 return Integer.MAX_VALUE;
-            }
 
-            if(sign * ans < Integer.MIN_VALUE) {
+            if(sign * ans < Integer.MIN_VALUE)
                 return Integer.MIN_VALUE;
-            }
+
             i++;
-        }
-        return (int)(sign*ans);
+            }
+            return (int)(sign * ans);
     }
 }
