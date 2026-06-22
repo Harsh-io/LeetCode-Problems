0521deaf450c98ecca213c549f016b6596bb7b24
Harsh Patial <harsh.work211@gmail.com>
2026-06-23 02:42:42 +0530

    Time: 1 ms (98.06%), Space: 42.7 MB (44.27%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,18 +1,11 @@
 class Solution {
     public int addDigits(int num) {
-    if(num < 10){return num;}
-    while(num >= 10){
-        num = findsum(num);
-    }
-    return num;
-    }
-    public int findsum(int n){
-        int sum = 0;
-        while(n > 0){
-            int r = n%10;
-            n = n/10;
-            sum = sum + r;
-        }
-        return sum;
+        if (num == 0) return 0;
+
+        if(num > 9){
+            num = num % 9;
+            if(num == 0) return 9;
+        } 
+        return num;
     }
 }
