5e3b33fc7e08071cb7400d2adcdd4d1ee479abc5
Harsh Patial <harsh.work211@gmail.com>
2026-06-30 21:30:11 +0530

    Time: 11 ms (91.6%), Space: 46.5 MB (30.71%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,28 +1,57 @@
-import java.util.*;
+//Brute Force
+// class Solution {
+//     public int numberOfSubstrings(String s) {
 
+//         int n = s.length();
+//         int total = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             int cntA = 0;
+//             int cntB = 0;
+//             int cntC = 0;
+//             for(int r = i; r < n; r++) {
+//                 char ch = s.charAt(r);
+//                 if(ch== 'a') cntA++;
+//                 else if(ch == 'b') cntB++;
+//                 else cntC++;
+
+//                 if(cntA >= 1 && cntB >= 1 && cntC >= 1){
+//                     total += n - r;
+//                     break;
+//                 }
+//             }
+//         }
+//         return total;
+//     }
+// }
 class Solution {
-
     public int numberOfSubstrings(String s) {
-        int n = s.length();
-        int left = 0;
-        int count = 0;
+        int l = 0;
+        int r = 0;
+        int cntA = 0; 
+        int cntB = 0; 
+        int cntC = 0; 
+        int total  = 0;
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int right = 0; right < n; right++) {
-            char ch = s.charAt(right);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        while(r < s.length()){
+            // add s[r] to count
+            char ch = s.charAt(r);
+            if(ch == 'a') cntA++;
+            else if(ch == 'b') cntB++;
+            else cntC++;
+             r++;
 
-            while (map.size() == 3) {
-                count += (n - right);
+            while(cntA >= 1 && cntB >= 1 && cntC >= 1) {
+            // remove s[l] from count
+            total += s.length() - r + 1;
 
-                char leftChar = s.charAt(left);
-                map.put(leftChar, map.get(leftChar) - 1);
-                if (map.get(leftChar) == 0) {
-                    map.remove(leftChar);
-                }
-                left++;
+            ch = s.charAt(l);
+            if(ch == 'a') cntA--;
+            else if(ch == 'b') cntB--;
+            else cntC--;
+            l++;
             }
         }
-        return count;
+        return total;
     }
-}
+}        
