4e6bd3079df0490ef8a92bbab59a8eaf9aea30cc
Harsh Patial <harsh.work211@gmail.com>
2026-07-02 00:14:19 +0530

    Time: 2 ms (74.12%), Space: 47.2 MB (33.98%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
@@ -1,13 +1,33 @@
+//BRUTE FORCE
+// class Solution {
+//     public int maximizeSum(int[] arr, int k) {
+//         Arrays.sort(arr);
+
+//         int n = arr.length;
+//         int ans = 0;
+
+//         for(int i=0; i<k; i++){
+//             ans += arr[n-1];
+//             arr[n-1] = arr[n-1] + 1;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int maximizeSum(int[] arr, int k) {
-        Arrays.sort(arr);
-
+        
+        int max = 0;
         int n = arr.length;
-        int ans = 0;
 
-        for(int i=0; i<k; i++){
-            ans += arr[n-1];
-            arr[n-1] = arr[n-1] + 1;
+        for(int i=0; i<n; i++){
+            max = Math.max(arr[i], max);
+        }
+
+        int ans = 0;
+        for(int j=0; j<k; j++){
+            ans += max;
+            max++;
         }
         return ans;
     }
