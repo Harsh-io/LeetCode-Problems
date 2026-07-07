9b7181e8796f91ad5fc50a037418e62e2fc51742
Harsh Patial <harsh.work211@gmail.com>
2026-07-07 22:08:28 +0530

    Time: 0 ms (100%), Space: 46.6 MB (21.92%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,36 +1,19 @@
 class Solution {
     public int[] shuffle(int[] arr, int n) {
+
         int[] ans = new int[arr.length];
+
         int i = 0;
         int j = n;
-        for(int k=0; k<arr.length; k++){
-            if(k%2==0){
-                ans[k] = arr[i];
-                i++;
-            }else{
-                ans[k] = arr[j];
-                j++;
-            }
+        int cnt = 0;
+
+        while( j < arr.length){
+            ans[cnt] = arr[i];
+            ans[cnt + 1] = arr[j];
+            i++;
+            j++;
+            cnt += 2;
         }
         return ans;
     }
 }
-
-//TRY Doing this  using swapping
-// class Solution {
-//     public int[] shuffle(int[] nums, int n) {
-//         for (int i = 0; i < n; i++) {
-//             int j = n + i;   // position of y[i]
-
-//             int temp = nums[j];
-
-//             // shift elements right to make space
-//             for (int k = j; k > 2 * i + 1; k--) {
-//                 nums[k] = nums[k - 1];
-//             }
-
-//             nums[2 * i + 1] = temp;
-//         }
-//         return nums;
-//     }
-// }
