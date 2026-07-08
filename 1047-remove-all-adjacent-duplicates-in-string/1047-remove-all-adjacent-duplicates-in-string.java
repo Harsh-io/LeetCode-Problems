9a1daae6631296324a03d4a524b60f9775f39b3e
Harsh Patial <harsh.work211@gmail.com>
2026-07-08 15:41:24 +0530

    Time: 11 ms (90.04%), Space: 47.4 MB (30.4%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,3 +1,38 @@
+// class Solution {
+//     public String removeDuplicates(String str) {
+//         if (str.length() <= 1) return str;
+
+//         StringBuilder sb = new StringBuilder();
+//         boolean found = false;
+
+//         int i = 0;
+
+//         while (i < str.length()) {
+//             int j = i + 1;
+
+//             while (j < str.length() && str.charAt(i) == str.charAt(j)) {
+//                 j++;
+//                 found = true;
+//             }
+
+//             // odd count means one char will remain
+//             if ((j - i) % 2 != 0) {
+//                 sb.append(str.charAt(i));
+//             }
+
+//             i = j;
+//         }
+
+//         String newStr = sb.toString();
+
+//         if (found) {
+//             return removeDuplicates(newStr);
+//         }
+
+//         return newStr;
+//     }
+// }
+
 class Solution {
     public String removeDuplicates(String s) {
         StringBuilder stack = new StringBuilder();
