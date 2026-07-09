4ba0c8815976f901eb0df3d10e6e93b551834345
Harsh Patial <harsh.work211@gmail.com>
2026-07-09 18:42:00 +0530

    Time: 303 ms (18.05%), Space: 272.7 MB (5.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,56 +1,45 @@
+import java.util.*;
+
 class Solution {
 
-    boolean solve(String s, int i, int j){
-        if(i >= j) return true;
+    int[][] dp = new int[1001][1001];
 
-        if(s.charAt(i) == s.charAt(j)){
-            return solve(s, i+1, j-1);
+    int solve(String s, int i, int j) {
+        if (i >= j) return 1;
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-        return false;
+
+        if (s.charAt(i) == s.charAt(j)) {
+            return dp[i][j] = solve(s, i + 1, j - 1);
+        }
+
+        return dp[i][j] = 0;
     }
+
     public String longestPalindrome(String s) {
         int n = s.length();
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
         int maxLen = 0;
         int sp = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j =i; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
 
-                if(solve(s, i, j) == true){
-                    if(j-i+1 > maxLen){
-                        maxLen = j-i+1;
+                if (solve(s, i, j) == 1) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
                         sp = i;
                     }
                 }
             }
         }
-        return s.substring(sp, sp+maxLen);
+
+        return s.substring(sp, sp + maxLen);
     }
 }
-
-// int n = s.length();
-    //     String ans = "";
-
-    //     for(int i=0; i<n; i++){
-
-    //         String odd = expand(s, i, i);
-    //         if(odd.length() > ans.length()){
-    //         ans = odd;
-    //         }
-
-    //         String even = expand(s, i, i+1);
-    //         if(even.length() > ans.length()){
-    //             ans = even;
-    //         }
-    //     }
-    //      return ans;
-    // }
-
-    // public String expand(String s, int l, int r){
-        
-    //     while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
-    //         l--;
-    //         r++;
-    //     }
-        
-    //     return s.substring(l+1,r);
