29f38a01144a6ab211d0008cef6c1a55715a023c
Harsh Patial <harsh.work211@gmail.com>
2026-07-10 19:19:06 +0530

    Time: 2 ms (96.45%), Space: 45.7 MB (51.14%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,29 +1,66 @@
+// class Solution {
+//     List<List<Integer>> ans = new ArrayList<>();
+
+//     public List<List<Integer>> combinationSum(int[] arr, int target) {
+//         List<Integer> temp = new ArrayList<>();
+//         generate(0, target, temp, arr);
+//         return ans;
+//     }
+
+//     public void generate(int i, int target, List<Integer> temp, int[] arr) {
+
+//         if (target == 0) {
+//             ans.add(new ArrayList<>(temp));
+//             return;
+//         }
+
+//         if (i == arr.length || target < 0) return;
+
+//         // take
+//         temp.add(arr[i]);
+//         generate(i, target - arr[i], temp, arr);
+
+//         // backtrack
+//         temp.remove(temp.size() - 1);
+
+//         // not take
+//         generate(i + 1, target, temp, arr);
+//     }
+// }
+
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] arr, int target) {
-        List<Integer> temp = new ArrayList<>();
-        generate(0, target, temp, arr);
+
+        ArrayList<Integer> sum = new ArrayList<>();
+        solve(0, arr, target, sum);
+
         return ans;
     }
 
-    public void generate(int i, int target, List<Integer> temp, int[] arr) {
+    public void solve(int idx, int[] arr, int t, ArrayList<Integer> sum){
 
-        if (target == 0) {
-            ans.add(new ArrayList<>(temp));
+        if(t == 0){
+           //ERROR FOR { ans.add(sum);} -> ans.add(sum) stores the same list reference if we change sum : ans also changes 
+           ans.add(new ArrayList<>(sum));
             return;
         }
 
-        if (i == arr.length || target < 0) return;
+        else if(t < 0 || idx == arr.length){
+            return ;
+        }
 
-        // take
-        temp.add(arr[i]);
-        generate(i, target - arr[i], temp, arr);
+        //take
+        sum.add(arr[idx]);
+        solve(idx, arr, t-arr[idx], sum);
+        sum.remove(sum.size()-1);
 
-        // backtrack
-        temp.remove(temp.size() - 1);
+        //not take
+        solve(idx+1, arr, t, sum);
 
-        // not take
-        generate(i + 1, target, temp, arr);
     }
 }
+
+
+// solve(idx, arr, t - arr[idx], sum.add(arr[idx])); -> here sum.add(arr[idx]) adds the element and returns true  -> But solve() expects an ArrayList<Integer>
