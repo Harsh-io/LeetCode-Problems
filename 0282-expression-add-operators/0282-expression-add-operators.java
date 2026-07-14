d6c094b4980d8b4ab24a458cc5bee1e2a45d80e8
Harsh Patial <harsh.work211@gmail.com>
2026-07-14 19:13:29 +0530

    Time: 53 ms (94.69%), Space: 47.1 MB (83.31%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,42 +1,56 @@
-//BODMAS : Multiply opr works from right to left 
-//DFS + Backtracking : generate all possible subseq of the num| i.e 1,2,3,12,23,123 
+//BODMAS : Multiply opr works from right to left
+//DFS + Backtracking : generate all possible subseq of the num| i.e 1,2,3,12,23,123
 //Keep in mind that leading zero number is not valid | ex:105 : i.e 05 is not a valid number
-// if we encounter * we need to do backtracking and re compute the ans
+//if we encounter * we need to do backtracking and re compute the ans
+
+import java.util.*;
 
 class Solution {
-    List<String> ans = new ArrayList<>();
-    public List<String> addOperators(String s, int target) {
 
-        helper(s , target, 0, "", 0, 0);
+    List<String> ans = new ArrayList<>();
+
+    public List<String> addOperators(String s, int target) {
+        helper(s, target, 0, new StringBuilder(), 0, 0);
         return ans;
     }
 
-    public void helper(String s, int target, int i, String path, long eval ,long residual){
-
-        if(i == s.length()){
-            if(eval == target){
-                ans.add(path);
+    public void helper(String s, int target, int i, StringBuilder path, long eval, long residual) {
+        if (i == s.length()) {
+            if (eval == target)
+                ans.add(path.toString());
                 return;
-            }
         }
 
-        String currstr = "";
         long num = 0;
 
-        //backtracking loop -> try all possible combinations
-        for(int j=i; j<s.length(); j++){
-            
+        // Try parsing numbers of different lengths starting from 'index'
+        for (int j = i; j < s.length(); j++) {
             //handle 0
-            if(j > i && s.charAt(i) == '0') return; //j>i means if there is a next element after 0 (eg 05) then invalid
-            currstr += s.charAt(j);
-            num = num*10 + s.charAt(j) - '0';
+            if (j > i && s.charAt(i) == '0') break; //j>i means if there is a next element after 0 (eg 05) then invalid
 
-            if(i == 0){ // if its the starting element there is nothing to add [eval or residual]
-                helper(s, target, j+1, path+currstr, num , num);
-            }else{
-                helper(s, target, j+1, path + "+" + currstr, eval +  num , num);
-                helper(s, target, j+1, path + "-" + currstr, eval - num , -num);//pass -num not num
-                helper(s, target, j+1, path + "*" + currstr, eval - residual + residual * num , residual * num);
+            num = num * 10 + (s.charAt(j) - '0');
+            String currstr = s.substring(i, j + 1);
+
+            //store current length so that we can backtrack later
+            int len = path.length();
+
+            if (i == 0) { //Position 0: The first number in the expression doesn't have a preceding operator
+                path.append(currstr);
+                helper(s, target, j + 1, path, num, num);
+                path.setLength(len); //backtrack
+
+            } else {
+                path.append("+").append(currstr);
+                helper(s, target, j + 1, path, eval + num, num);
+                path.setLength(len); //backtrack
+
+                path.append("-").append(currstr);
+                helper(s, target, j + 1, path, eval - num, -num); //pass -num not num
+                path.setLength(len); //backtrack
+
+                path.append("*").append(currstr);
+                helper(s, target, j + 1, path,eval - residual + residual * num,residual * num);
+                path.setLength(len); //backtrack
             }
         }
     }
