dcba2d798b5795c95ee86b0139f24c29e2aaebba
Harsh Patial <harsh.work211@gmail.com>
2026-07-20 04:29:11 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.74%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+
+        int sum =0;
+        int maxsum = INT_MIN;
+        for (auto it:nums) {
+
+            sum += it;
+
+            maxsum = max(sum , maxsum);
+
+            if (sum < 0) sum =0;
+
+
+        }
+
+        return maxsum;
+
+    }
+};
