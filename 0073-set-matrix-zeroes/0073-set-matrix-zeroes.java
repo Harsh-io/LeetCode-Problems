908b145f247b67a8a44b4eb690c243dd6fd020d0
Harsh Patial <harsh.work211@gmail.com>
2026-07-25 00:24:33 +0530

    Time: 1 ms (98.07%), Space: 47.7 MB (43.46%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void setZeroes(int[][] mat) {
+        
+        boolean[] rows = new boolean[mat.length];
+        boolean[] cols = new boolean[mat[0].length];
+
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+
+                if(mat[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+
+                if(rows[i] || cols[j]){
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+}
