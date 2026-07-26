a7350484419e8a6c4f7ff351471d5778a323202b
Harsh Patial <harsh.work211@gmail.com>
2026-07-27 03:23:55 +0530

    Time: 1 ms (59.71%), Space: 48.5 MB (31.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,31 +1,32 @@
+import java.util.*;
 class Solution {
     public int trap(int[] arr) {
+        int n = arr.length;
+        int totalWater = 0;
+        int maxLeft = 0;
+        int maxRight = 0;
+
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
         
-        int leftMax = 0, rightMax = 0;
-        int total = 0;
-        int l = 0, r = arr.length-1;
-
-        while(l < r){
-
-            if(arr[l] <= arr[r]){
-
-                if(leftMax > arr[l]){
-                    total += leftMax  - arr[l];
-                }else{
-                    leftMax = arr[l];
-                }
-                l++;
-            }else{
-                if(rightMax > arr[r]){
-                    total += rightMax - arr[r];
-                }else{
-                    rightMax = arr[r];
-                }
-                r--;
-            }
+        prefixMax[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = Math.max(prefixMax[i-1], arr[i]);
         }
-        return total;
+
+        suffixMax[n-1] = arr[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            suffixMax[i] = Math.max(suffixMax[i+1], arr[i]);
+        }
+
+        for (int j = 0; j < n; j++) {
+            maxLeft = prefixMax[j];
+            maxRight = suffixMax[j];
+
+            if(arr[j] < maxLeft && arr[j] < maxRight){
+                totalWater += Math.min(maxLeft, maxRight) - arr[j];
+            }
+        } 
+        return totalWater;
     }
 }
-//TC: O(N)
-//SC: O(1)
