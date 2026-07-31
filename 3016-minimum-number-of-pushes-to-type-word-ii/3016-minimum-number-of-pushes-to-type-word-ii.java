833039cc71198c2f558cdbd0a4766c895510be6c
Harsh Patial <harsh.work211@gmail.com>
2026-07-31 20:25:09 +0530

    Time: 117 ms (5.49%), Space: 48 MB (52.6%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumPushes(String s) {
+        Integer [] arr = new Integer[26];
+        Arrays.fill(arr, 0);
+        int sum = 0;
+
+        for(int i=0; i<s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int push = 1;
+
+        for(int i=0; i<26; i++){
+
+            if(arr[i] == 0) break;
+
+            if(i > 0 && i%8 == 0) push += 1;
+
+            sum += arr[i] * push;
+
+            System.out.println(arr[i]);
+            System.out.println(push);
+            System.out.println(sum);
+            System.out.println();
+
+            
+        }
+        return sum;
+    }
+}
