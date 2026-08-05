467a4f8cdcb3c3b818472cacea5dceff65373e4c
Harsh Patial <harsh.work211@gmail.com>
2026-08-05 22:14:27 +0530

    Time: 1 ms (98.5%), Space: 42.4 MB (92.33%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,20 +1,11 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
+        int ans = 0 ;
 
-        while (n > 0){
-            int r = n % 10;
-            n = n / 10;
-            map.put(r, map.getOrDefault(r, 0)+1);
+        while ( n > 0 ){
+            ans += n % 10 ;
+            n = n/10 ;
         }
-
-        for(Map.Entry<Integer, Integer> e : map.entrySet()){
-
-            sum += e.getKey() * e.getValue();
-        }
-
-        return sum;
+        return ans ;
     }
 }
