ef04fa0b99e83e52ba4bc58fb2ec7af407b564b8
Harsh Patial <harsh.work211@gmail.com>
2026-08-06 23:13:25 +0530

    Time: 36 ms (35.58%), Space: 47 MB (74.99%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,51 +1,25 @@
-// class Solution {
-//     public String removeDuplicates(String str) {
-//         if (str.length() <= 1) return str;
-
-//         StringBuilder sb = new StringBuilder();
-//         boolean found = false;
-
-//         int i = 0;
-
-//         while (i < str.length()) {
-//             int j = i + 1;
-
-//             while (j < str.length() && str.charAt(i) == str.charAt(j)) {
-//                 j++;
-//                 found = true;
-//             }
-
-//             // odd count means one char will remain
-//             if ((j - i) % 2 != 0) {
-//                 sb.append(str.charAt(i));
-//             }
-
-//             i = j;
-//         }
-
-//         String newStr = sb.toString();
-
-//         if (found) {
-//             return removeDuplicates(newStr);
-//         }
-
-//         return newStr;
-//     }
-// }
-
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuilder stack = new StringBuilder();
 
-        for (char c : s.toCharArray()) {
-            int len = stack.length();
-            if (len > 0 && stack.charAt(len - 1) == c) {
-                stack.deleteCharAt(len - 1); // remove the top of the stack
-            } else {
-                stack.append(c); // push current character onto the stack
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            
+            boolean dup = false;
+
+            while(!st.isEmpty() && st.peek() == ch){
+                dup = true;
+                st.pop();
             }
+            
+            if(!dup) st.push(ch);
         }
 
-        return stack.toString();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
