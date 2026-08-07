0a26ff55b142435926a520498b29ae45cae9ce73
Harsh Patial <harsh.work211@gmail.com>
2026-08-08 03:20:19 +0530

    Time: 1 ms (92.94%), Space: 47.4 MB (16.01%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(int[] arr) {
+        
+        int n = arr.length;
+        int prefix = 1;
+        int suffix = 1;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= arr[i];
+            suffix *= arr[n-1-i];
+
+            max = Math.max(max, Math.max(prefix, suffix));
+        }
+
+        return max;
+    }
+}
