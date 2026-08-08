84d293793951663193bf94743ce46d34ea1d7ab6
Harsh Patial <harsh.work211@gmail.com>
2026-08-09 01:16:50 +0530

    Time: 372 ms (5.11%), Space: 49.1 MB (97.8%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] str) {
+
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] vis = new boolean[str.length];
+
+        for (int i = 0; i < str.length; i++) {
+
+            if (vis[i])
+                continue;
+
+            vis[i] = true;
+
+            List<String> group = new ArrayList<>();
+            group.add(str[i]);
+
+            int[] f1 = new int[26];
+
+            for (int x = 0; x < str[i].length(); x++) {
+                char ch = str[i].charAt(x);
+                f1[ch - 'a']++;
+            }
+
+            for (int j = i + 1; j < str.length; j++) {
+
+                if (vis[j])
+                    continue;
+
+                if (str[i].length() != str[j].length())
+                    continue;
+
+                int[] f2 = new int[26];
+
+                for (int y = 0; y < str[j].length(); y++) {
+                    char ch = str[j].charAt(y);
+                    f2[ch - 'a']++;
+                }
+
+                boolean same = true;
+
+                for (int m = 0; m < 26; m++) {
+                    if (f1[m] != f2[m]) {
+                        same = false;
+                        break;
+                    }
+                }
+
+                if (same) {
+                    group.add(str[j]);
+                    vis[j] = true;
+                }
+            }
+
+            ans.add(group);
+        }
+
+        return ans;
+    }
+}
