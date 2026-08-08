2320a78f5cdc6d3d4ad98aeb0e13f4d85e0cbda5
Harsh Patial <harsh.work211@gmail.com>
2026-08-09 00:37:47 +0530

    Time: 4 ms (87.88%), Space: 46.8 MB (35.2%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,30 +1,33 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) return s;
+        
+        if(numRows == 1 || s.length() <= numRows) return s;
 
         StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {
+
+        for(int i=0; i<numRows; i++){
             rows[i] = new StringBuilder();
         }
 
-        int currentRow = 0;
-        boolean goingDown = false;
+        int curr = 0;
+        boolean goingdown = false;
 
-        for (char c : s.toCharArray()) {
-            rows[currentRow].append(c);
+        for(char ch : s.toCharArray()){
 
-            if (currentRow == 0 || currentRow == numRows - 1) {
-                goingDown = !goingDown;
+            rows[curr].append(ch);
+
+            if(curr == 0 || curr == numRows-1){
+                goingdown = !goingdown;
             }
 
-            currentRow += goingDown ? 1 : -1;
+            curr +=  goingdown ? 1 : -1;
         }
 
         StringBuilder result = new StringBuilder();
         for (StringBuilder row : rows) {
             result.append(row);
         }
+        return  result.toString();
 
-        return result.toString();
     }
 }
