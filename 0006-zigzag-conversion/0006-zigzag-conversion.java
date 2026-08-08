0e4f7a91cc6cee7e421979c11374d6789670dc51
Harsh Patial <harsh.work211@gmail.com>
2026-08-09 00:27:51 +0530

    Time: 4 ms (87.88%), Space: 44.9 MB (99.97%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,61 +1,30 @@
 class Solution {
-    public String convert(String s, int n) {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) return s;
 
-        if(n == 1 || s.length() <= n)return s;
-        StringBuilder[] row = new StringBuilder[n];
-
-        for(int i=0; i<n; i++){
-            row[i] = new StringBuilder();
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
         }
 
-        boolean down = false;
-        int i = 0;
+        int currentRow = 0;
+        boolean goingDown = false;
 
-        for(char c : s.toCharArray()){
-            row[i].append(c);
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
 
-            if(i == 0 || i == n-1){
-                down = !down;
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
             }
 
-            i += down ? 1 : -1;
+            currentRow += goingDown ? 1 : -1;
         }
 
-        StringBuilder res = new StringBuilder();
-        for(int j=0; j<n; j++){
-            res.append(row[j]);
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
         }
 
-        return res.toString();
+        return result.toString();
     }
 }
-
-
-
-//  if (numRows == 1 || s.length() <= numRows) return s;
-
-    //     StringBuilder[] rows = new StringBuilder[numRows];
-    //     for (int i = 0; i < numRows; i++) {
-    //         rows[i] = new StringBuilder();
-    //     }
-
-    //     int currentRow = 0;
-    //     boolean goingDown = false;
-
-    //     for (char c : s.toCharArray()) {
-    //         rows[currentRow].append(c);
-
-    //         if (currentRow == 0 || currentRow == numRows - 1) {
-    //             goingDown = !goingDown;
-    //         }
-
-    //         currentRow += goingDown ? 1 : -1;
-    //     }
-
-    //     StringBuilder result = new StringBuilder();
-    //     for (StringBuilder row : rows) {
-    //         result.append(row);
-    //     }
-
-    //     return result.toString();
-    // }
