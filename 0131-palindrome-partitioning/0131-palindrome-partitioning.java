6e90114ee2c686d73f275ef18da9e72b7d742c08
Harsh Patial <harsh.work211@gmail.com>
2026-08-10 21:44:16 +0530

    Time: 13 ms (7.98%), Space: 66 MB (6.3%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,48 +1,40 @@
-///CANT USE:  take and NotTake Approach because  we dont need subsequences we need substrings : this appraoch may skip characters 
-
 class Solution {
-
+   
     List<List<String>> ans = new ArrayList<>();
 
     public List<List<String>> partition(String s) {
 
-        if(s.length() == 0 ) return ans;
+        if(s.length() == 0) return ans;
+
         generate(0, s, new ArrayList<>());
+
         return ans;
     }
 
     public void generate(int i, String s, ArrayList<String> arr){
+
 
         if(i == s.length()){
             ans.add(new ArrayList<>(arr));
             return;
         }
 
-        for(int j = i; j<s.length(); j++){
+        for(int j=i; j<s.length(); j++){
+
             String str = s.substring(i, j+1);
 
-            if(palindrome(str)){
+            if(isPalindrome(i, j, str)){
                 arr.add(str);
 
                 generate(j+1, s, arr);
 
-                arr.remove(arr.size() - 1);
+                arr.remove(arr.size()-1);
             }
         }
     }
 
-    public boolean palindrome(String str) {
-        int i = 0;
-        int j = str.length() - 1;
+    public boolean isPalindrome(int i, int j, String str){
 
-        while (i < j) {
-            if (str.charAt(i) != str.charAt(j))
-                return false;
-
-            i++;
-            j--;
-        }
-
-        return true;
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
 }
