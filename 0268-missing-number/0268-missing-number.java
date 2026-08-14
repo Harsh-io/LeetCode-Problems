c6607ea067876bd294e7b20ec7a7ef094d16a499
Harsh Patial <harsh.work211@gmail.com>
2026-08-14 23:53:03 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,24 +1,11 @@
 class Solution {
     public int missingNumber(int[] arr) {
         
-        int i = 0;
-        while(i < arr.length){
+        int ans = arr.length;
+        for(int i=0; i<arr.length; i++){
 
-            int correct = arr[i];
-            if(arr[i] < arr.length && arr[i] != arr[correct]){ // is the element at its index : index = val
-                int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
-            }
-            else i++;
+            ans ^= i ^ arr[i]; 
         }
-
-        for(int j=0; j<arr.length; j++){
-            if( j != arr[j]){
-                return j;
-            }
-        }
-
-        return arr.length;
+        return ans;
     }
 }
