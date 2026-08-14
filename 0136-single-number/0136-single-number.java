517a1a1bc64b9af85e747e9cc0523ec01b12b218
Harsh Patial <harsh.work211@gmail.com>
2026-08-14 23:38:34 +0530

    Time: 1 ms (99.94%), Space: 46.9 MB (46.59%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        
+        int ans = 0;
+
+        for(int num : arr){
+            ans = ans ^ num;
+        }
+
+        return ans;
+    }
+}
